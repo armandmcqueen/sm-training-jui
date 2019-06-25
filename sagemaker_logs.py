0014df89b1149801d60f00daab62e2f9eff7ea0c
Rahul Huilgol <huilgolr@amazon.com>
2019-06-25 15:40:48 -0700

    thread with log stream

--- sagemaker_logs.py
+++ sagemaker_logs.py
@@ -1,37 +1,43 @@
+import ipywidgets as widgets
 import boto3
 import time
 from collections import deque
-from IPython.display import clear_output, display
+from IPython.display import clear_output
 
 SM_CW_LOG_GROUP = "/aws/sagemaker/TrainingJobs"
 
-region = "us-east-1"
-sm_job = "armand-vgg-example-2019-06-24-22-12-16-025"
-hostname = "algo-1"
+class WidgetLogger:
+    def __init__(self, out_widget):
+        self.max_lines = 15
+        self.tail_buffer = deque([], maxlen=self.max_lines)
+        self.out_widget = out_widget
 
+    def logline_handler(self, line):
+        if 'errno' in line or 'inputs.nvidia_smi' in line:
+            return
+        line = line.replace('#011','\t')
+        self.tail_buffer.append(line)
+        with self.out_widget:
+            clear_output(wait=True)
+            print('\n'.join(self.tail_buffer))
 
-def horovod_logline_handler(line):
-    if 'errno' in line:
-        pass
-    else:
-        # line = line.replace("#011", '\t')
-        print(line)
+def get_wlogger():
+    out = widgets.Output(layout={'border': '1px solid black'})
+    wlogger = WidgetLogger(out)
+    return out, wlogger
 
-
-
-def tail_sm_logs(sm_job_name, hostname, region="us-east-1", logline_handler=horovod_logline_handler):
-
+def tail_sm_logs(sm_job_name, hostname, logline_handler, region="us-east-1"):
     log_client = boto3.session.Session(region_name=region).client('logs')
     response = log_client.describe_log_streams(
         logGroupName=SM_CW_LOG_GROUP,
         logStreamNamePrefix=sm_job_name,
         orderBy='LogStreamName',
     )
-
+    
     log_stream_prefix = f'{sm_job_name}/{hostname}-'
-    log_stream_name = [s['logStreamName']
-                       for s
-                       in response['logStreams']
+    log_stream_name = [s['logStreamName'] 
+                       for s 
+                       in response['logStreams'] 
                        if s['logStreamName'].startswith(log_stream_prefix)]
 
     assert len(log_stream_name) == 1, "There should only be one matching logstream"
@@ -60,28 +66,12 @@ def tail_sm_logs(sm_job_name, hostname, region="us-east-1", logline_handler=horo
 
         # last_forward_token = next_forward_token
         next_forward_token = response["nextForwardToken"]
-        time.sleep(0.5)
+        time.sleep(0.5)    
 
-
-class JupyterFixedMemoryLogger:
-    def __init__(self):
-        self.max_lines = 300
-        self.pagination_overlap_lines = 50
-        self.tail_buffer = deque([], maxlen=self.pagination_overlap_lines)
-        self.output_length = 0
-
-    def logline_handler(self, line):
-        if 'errno' in line:
-            return
-
-        self.tail_buffer.append(line)
-        self.output_length += 1
-        print(line)
-
-        if self.output_length == self.max_lines:
-            clear_output()
-            for l in self.tail_buffer:
-                print(l)
-
-            self.tail_buffer.clear()
-            self.output_length = self.pagination_overlap_lines
+def tail_log_task(sm_job_name, hostname, region='us-east-1'):
+    from IPython.display import display
+    out, wlogger = get_wlogger()
+    display(out)
+    import threading
+    thread = threading.Thread(target=tail_sm_logs, args=(sm_job_name, hostname, wlogger.logline_handler, region))
+    thread.start()
